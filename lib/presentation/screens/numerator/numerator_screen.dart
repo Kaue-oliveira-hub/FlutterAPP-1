3aae5d7b020e6201ef93fa04f525b62e37888408
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-05-15 19:17:26 +0200

    Statefullwidget

--- lib/presentation/screens/numerator/numerator_screen.dart
+++ lib/presentation/screens/numerator/numerator_screen.dart
@@ -1,8 +1,50 @@
 import  'package:flutter/material.dart';
 
 
+class NumeratorScreen extends StatefulWidget{
+const NumeratorScreen({super.key});
+
+@override
+  State<NumeratorScreen> createState() => _NumeratorScreenState();
+}
+
+class _NumeratorScreenState extends State<NumeratorScreen>{
+int counter = 0;
+
+
+@override
+Widget build(BuildContext context){
+return Scaffold(
+        appBar: AppBar(
+          title: Text('Numerator Screen'),
+        ),
+       body: Center(
+        child: Text('Valor: $counter', style: Theme.of(context).textTheme.titleLarge,),
+        ),
+
+    floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            counter++;
+          setState(() {
+           counter++;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        );
+}
+
+}
+
+
+
+
+/*
 class NumeratorScreen extends StatelessWidget{
-    const NumeratorScreen({super.key});
+
+  int counter = 0;
+
+     NumeratorScreen({super.key});
 
 @override
     Widget build(BuildContext context) {
@@ -11,10 +53,18 @@ class NumeratorScreen extends StatelessWidget{
           title: Text('Numerator Screen'),
         ),
        body: Center(
-        child: Text('Valor: 10', style: Theme.of(context).textTheme.titleLarge,),
-        )
+        child: Text('Valor: $counter', style: Theme.of(context).textTheme.titleLarge,),
+        ),
+    floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            counter++;
+            print('Counter: $counter');
+          },
+          child: Icon(Icons.add),
+        ),
       );
         
     }
 
 }
+*/ 
